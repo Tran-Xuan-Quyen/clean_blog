8c20d9876acdec0f482964f35639b5c168a92500
Tran-Xuan-Quyen <tranquyen15122003@gmail.com>
2023-04-08 00:11:48 +0700

    clean_blog_NodeJs

---
+++ controllers/redirectAuthMiddleware.js
@@ -0,0 +1,5 @@
+const User = require('../models/User')
+module.exports = (req, res, next) => {
+    if(req.session.userId) return res.redirect('/')
+    next()
+}
---
+++ test.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const BlogPost = require('./models/BlogPost');
+
+mongoose.connect('mongodb://localhost:27017/test_my_database')
+
+BlogPost.create({
+    title: 'hello bạn',
+    body: 'bạn khỏe không'
+}, (error, blogpost) => {
+    console.log(error, blogpost);
+})
+//tạo document
+BlogPost.create({
+    title: 'hi',
+    body: 'nothing'
+}, (error, blogpost) => {
+    console.log(error, blogpost);
+})
+//find
+BlogPost.find({}, (error, blogpost) => {
+    console.log(error, blogpost);
+})
+
+//update
+var id = "63b296a76a30bc41c34bfcf7";
+BlogPost.findByIdAndUpdate(id, (error, blogpost) => {
+    title: 'updated file'
+}, (error, blogpost) => {
+    console.log(error, blogpost);
+})
