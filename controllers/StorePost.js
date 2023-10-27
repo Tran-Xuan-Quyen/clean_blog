b5462f2d2dbe2ed55b1b9b17fa98595070d3b87d
Tran-Xuan-Quyen <tranquyen15122003@gmail.com>
2023-10-27 11:09:08 +0700

    update

--- controllers/StorePost.js
+++ controllers/StorePost.js
@@ -1,11 +1,22 @@
 const BlogPost = require('../models/BlogPost.js')
 const path = require('path')
-
+const User = require('../models/User.js')
 module.exports = (req, res) => {
     let image = req.files.image;
+    var ObjectId = require('mongodb').ObjectId;
+    req.session.userId = new ObjectId(req.session.userId);
     image.mv(path.resolve(__dirname, '/public/upload', image.name), function(err) {
-        BlogPost.create({...req.body, image: '/upload/' + image.name}, function(error) {
-            res.redirect('/')
-        })
+        let usernameSession;
+        User.findOne( { _id :  req.session.userId}, (err, user) => {
+            usernameSession = user.username;  
+            BlogPost.create({
+                username: usernameSession,
+                title: req.body.title,
+                body: req.body.body,    
+                image: '/upload/' + image.name, }, function(error, blogpost) {
+                res.redirect('/')
+            })
+        });
+       
     })
 }
--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,8 @@
 const BlogPost = require('../models/BlogPost.js')
 module.exports = (req, res) => {
     BlogPost.find({}, function(error, posts) {
-        console.log(req.session)
-        console.log(posts);
+        console.log(req.session);
+        //console.log(posts);
         res.render('index', {blogposts: posts})
     })
 }
